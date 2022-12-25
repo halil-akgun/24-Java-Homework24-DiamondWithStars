c76769f356718eb95a1e36fecad898026b2baa6b
halil <halilakgun0@gmail.com>
2022-12-25 23:04:15 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Ödev
+        Java'da döngüler kullanarak yıldızlar ile elmas yapınız.
+        */
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = input.nextInt();
+        for (int i = 1; i <= a; i++) {
+            for (int j = i; j < a; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
