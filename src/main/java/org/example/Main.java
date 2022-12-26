3f20a834aaf566e43370c8976afe351ebe6cc6e0
halil <halilakgun0@gmail.com>
2022-12-26 14:16:04 +0300

    triangle --> diamond

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,9 +11,19 @@ public class Main {
         */
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number: 2x");
         int a = input.nextInt();
+
         for (int i = 1; i <= a; i++) {
+            for (int j = i; j < a; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = a; i >= 1; i--) {
             for (int j = i; j < a; j++) {
                 System.out.print(" ");
             }
